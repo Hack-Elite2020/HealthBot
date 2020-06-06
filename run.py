6c549a5134a9e305a8e4f136a518541a9289e695
pallavi <pallavi.gutta@optum.com>
2020-06-07 00:56:54 +0530

    final_portnumber

--- run.py
+++ run.py
@@ -37,7 +37,6 @@ def chatbot():
 def get_bot_response():
     userText = request.args.get('msg')
     response = str(english_bot.get_response(userText))
-
     appendfile=os.listdir('saved_conversations')[-1]
     appendfile= open('saved_conversations/'+str(filenumber),"a")
     appendfile.write('user : '+userText+'\n')
@@ -48,4 +47,4 @@ def get_bot_response():
 
 
 if __name__ == "__main__":
-    app.run(host ='0.0.0.0', port = 8000, debug = True)
+    app.run(host ='0.0.0.0', port = 8080, debug = True)
